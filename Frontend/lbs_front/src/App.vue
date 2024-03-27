2ed87249a7a0cd55546f71fb04ef285c6afdb894
IceCreamPie-dev <icecreampie9000@gmail.com>
2024-03-27 21:24:36 +0900

    FIX [프론트] 로그인 예제

--- Frontend/lbs_front/src/App.vue
+++ Frontend/lbs_front/src/App.vue
@@ -2,9 +2,43 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <div>
+    <input v-model="username" type="text" placeholder="Username">
+    <input v-model="password" type="password" placeholder="Password">
+    <button @click="login">Login</button>
+    </div>
   </nav>
   <router-view/>
 </template>
+
+<script>
+import axios from 'axios';
+import { API_URL } from './config.js';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const response = await axios.post(`${API_URL}/login`, {
+          username: this.username,
+          password: this.password
+        });
+
+        // Store the returned token in local storage (or wherever you want)
+        localStorage.setItem('authToken', response.data.token);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+};
+</script>
 
 <style>
 #app {
