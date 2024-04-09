e03fd6c1dfb30faf8299f4a48071d99d2448f80b
IceCreamPie-dev <icecreampie9000@gmail.com>
2024-04-09 15:39:27 +0900

    Refactor [백엔드] - QnA 게시물 조회 쿼리 in main.js

--- Backend/main.js
+++ Backend/main.js
@@ -256,7 +256,7 @@ app.get('/board/QnA', (req, res) => {
   const offset = (page - 1) * limit;
 
   // QnA게시물 목록 조회
-  tomysql.query(`SELECT * FROM QnA LIMIT ? OFFSET ?`, [ +limit, offset], (err, results) => {
+  tomysql.query(`SELECT qid, title, created_at FROM QnA ORDER BY created_at DESC LIMIT ? OFFSET ?`, [ +limit, offset], (err, results) => {
     if (err) {
       console.error('[오류] 게시물 조회중 오류 발생:', err);
       res.status(500).json({ error: '내부 서버 오류' });
