3d19e7f2f64e6d1a3f77fde8c320aa7e4503518d
IceCreamPie-dev <icecreampie9000@gmail.com>
2024-04-10 12:17:19 +0900

    ADD [프론트] - pm2 설정

--- Frontend/lbs_front/ecosystem.js
+++ Frontend/lbs_front/ecosystem.js
@@ -1,6 +1,7 @@
 module.exports = {
     apps: [
       {
+        watch: true,
         name: "LBS_Frontend",
         script: "./node_modules/@vue/cli-service/bin/vue-cli-service.js",
         args: "serve"
