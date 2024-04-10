d125e11089177caf1be18eace7a6abc5f891e40e
IceCreamPie <58936819+IceCreamPie-dev@users.noreply.github.com>
2024-04-10 11:41:29 +0900

    Delete Frontend directory

--- Frontend/lbs_front/ecosystem.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apps: [
-      {
-        name: "LBS_Frontend",
-        script: "./node_modules/@vue/cli-service/bin/vue-cli-service.js",
-        args: "serve"
-      }
-    ]
-  };
