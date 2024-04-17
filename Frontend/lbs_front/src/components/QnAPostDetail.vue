695541a95bf6b094d0abb585fd4305754edf234b
BSN-Pcj <115200511+BSN-Pcj@users.noreply.github.com>
2024-04-18 01:06:33 +0900

    QnA 간략추가

--- Frontend/lbs_front/src/components/QnAPostDetail.vue
+++ Frontend/lbs_front/src/components/QnAPostDetail.vue
@@ -1,33 +1,48 @@
 <template>
-    <div>
-      <h2>{{ post.title }}</h2>
-      <p>{{ post.content }}</p>
-      <p>작성일: {{ post.createdAt }}</p>
-    </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        post: {},
-      };
+  <div>
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.content }}</p>
+    <p>작성일: {{ post.createdAt }}</p>
+    <button @click="goBack">목록으로 돌아가기</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+props: ['postId'],
+  data() {
+    return {
+      post: {},
+    };
+  },
+  created() {
+    this.fetchPost();
+  },
+mounted() {
+  window.addEventListener('keydown', this.handleKeydown);
+},
+beforeUnmount() {
+  window.removeEventListener('keydown', this.handleKeydown);
+},
+  methods: {
+    async fetchPost() {
+      try {
+        const response = await axios.get(`/api/board/QnA/${this.postId}`);
+        this.post = response.data;
+      } catch (error) {
+        console.error(error);
+      }
     },
-    created() {
-      const postId = this.$route.params.postId;
-      this.fetchPost(postId);
-    },
-    methods: {
-      async fetchPost(postId) {
-        try {
-          const response = await axios.get(`/api/board/QnA/${postId}`);
-          this.post = response.data;
-        } catch (error) {
-          console.error(error);
-        }
-      },
-    },
-  };
-  </script>
+  goBack() {
+    this.$emit('goBack');
+  },
+  handleKeydown(event) {
+    if (event.key === 'Escape') {
+      this.goBack();
+    }
+  },
+  },
+};
+</script>
