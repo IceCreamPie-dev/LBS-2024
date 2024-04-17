de57529f9351e9f5060b0e58989500470b1236de
ohsoeun <134242691+ohsoeun@users.noreply.github.com>
2024-04-18 01:34:46 +0900

    공지사항
    게시물 생성버튼
    공지사항 상세페이지
    공지사항 생성페이지

--- Frontend/lbs_front/src/components/GoggiPostDetail.vue
+++ Frontend/lbs_front/src/components/GoggiPostDetail.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <h2>{{ post.title }}</h2>
-    <p>{{ post.content }}</p>
-    <button @click="goBack">목록으로 돌아가기</button>
+  <div class="post-details">
+    <form class="post-form">
+      <h2 class="post-title">{{ post.title }}</h2>
+      <p class="post-content">{{ post.content }}</p>
+    </form>
+    <div class="post-btn">
+    <button @click="goBack" class="back-button">목록으로 돌아가기</button>
+  </div>
   </div>
 </template>
 
@@ -45,3 +49,66 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.post-details {
+  max-width: 100%;
+  height: 90%;
+  margin: 0 auto;
+  margin-top: 2.7%;
+  padding: 1%; 
+  background-color: #fff;
+  border-radius: 10px; 
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); 
+}
+
+.post-form{
+  margin: 0 auto; 
+  margin: 2%;
+  width: 92%;
+  height: 90%;
+  color: #605548;
+  padding: 1.5%;
+  border-radius: 10px;
+  font-family: 'NanumSquareNeo';
+
+}
+.post-title {
+  font-size: 24px; 
+  padding: 1%;
+  font-weight: bold; 
+  color: #605548;
+  margin-bottom: 2%;
+  border-bottom: #FFDE80 3px solid;
+}
+
+.post-content {
+  font-size: 16px; 
+  height: 90%;
+  color: #80776D; 
+  margin-bottom: 20px; 
+}
+
+.post-btn{
+
+  text-align: center;
+}
+
+.back-button {
+  display: inline-block;
+  font-family: 'NanumSquareNeobold';
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #FFDE80;
+  color: #605548;
+  border: none; 
+  border-radius: 5px;
+  cursor: pointer; 
+  transition: background-color 0.3s; /* 호버 효과 애니메이션 */
+}
+
+.back-button:hover {
+  background-color: #FFCE55; /* 호버 시 배경색 변경 */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); 
+}
+</style>
--- Frontend/lbs_front/src/components/gonggiMonit.vue
+++ Frontend/lbs_front/src/components/gonggiMonit.vue
@@ -5,7 +5,7 @@
   </div>
   <div class="yellow-line"></div>
   <div v-if=isListMode>
-    <div v-if=role> <!-- 관리자 게시물 생성 기능이 보임 왼쪼 정렬-->
+    <div v-if=role class="post-add-button-container"> <!-- 관리자 게시물 생성 기능이 보임 왼쪼 정렬-->
     <button class="post-add-button" @click="clickAddPostMod">게시물 생성</button>
   </div>
     <div v-for="post in post" :key="post.iid">
@@ -17,18 +17,28 @@
     <GoggiPostDetail :post-id="selectedPostID" @goBack="goBack"/>
   </div>
   <div v-else-if=isCreateMode>
-    테스트
+   
     <form @submit.prevent="submitPost">
+      <div class="container">
+
+       <div class="post-form">
+
       <div class="form-group">
-        <label for="title">제목</label>
+        <label for="title" class="title-label_post">제목</label>
         <input type="text" id="title" v-model="title" class="form-control" placeholder="제목을 입력하세요">
       </div>
-      <div class="form-group">
-        <label for="content">내용</label>
-        <textarea id="content" v-model="content" class="form-control" placeholder="내용을 입력하세요"></textarea>
+      <div class="form-group2">
+        <label for="content" class="content-label">내용</label>
+        <textarea id="content" v-model="content" rows="15" required class="form-control" placeholder="내용을 입력하세요"></textarea>
       </div>
-      <button type="submit" class="btn btn-primary" @click="CreatePost">등록</button>
-      <button type="button" class="btn btn-secondary" @click="goBack">취소</button>
+      <div class="button-group">
+      <button type="submit" class="btn-primary" @click="CreatePost">등록</button>
+      <button type="button" class="btn-secondary" @click="goBack">취소</button>
+      </div>
+
+    </div>
+
+    </div>
     </form>
   </div>
 </template>
@@ -121,16 +131,143 @@ export default {
   margin-bottom: 5px ;
 }
 
+
+
+.post-add-button-container {
+    text-align: right; /* 내부 요소를 오른쪽으로 정렬합니다. */
+
+}
 .post-add-button{
   /* 게시물 생성 버튼 */
-  background-color: #FFD700;
+  background-color: #FFDE80;
   color: #484848;
   border: none;
   border-radius: 3px;
+  margin-top: 1%;
+  margin-right: 3%;
   padding: 10px;
-  margin-left: 1vw;
   cursor: pointer;
-  font-family: 'NanumSquareNeo';
-  font-size: 1vw;
+  font-family: 'NanumSquareNeobold';
+  font-size: 17px;
+  display: inline-block;
+  transition: box-shadow 0.3s ease; /* 버튼을 블록 요소로 표시하여 왼쪽 정렬을 위해 줄 바꿈됩니다. */ /* 왼쪽 여백을 0으로 설정하여 왼쪽 정렬합니다. */
 }
+
+.post-add-button:hover{
+  background-color: #ffd358;
+  color: #484848;
+  box-shadow: 2px 2px 4px 1px rgba(0, 0, 0, 0.2);
+  
+  }
+/*------------------------------------------------------------------------------------------ */
+  .container {
+    max-width: 100%;
+    height: 90%;
+    margin: 0 auto;
+  }
+
+  .post-form {
+    margin: 0 auto; 
+    margin: 2%;
+    width: 92%;
+    height: 90%;
+    background-color:#ffffff;
+    color: #605548;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 1px 2px 5px 2px rgba(0, 0, 0, 0.2);
+    font-family: 'NanumSquareNeo';
+  }
+  .form-group {
+    border-radius: 50px;
+    padding: 1.5%;
+    padding-bottom: 3%;
+  }
+  .form-group2 {
+    margin-top: 15px;
+    margin-bottom: 30px;
+  }
+
+  .title-label_post{
+    display: inline-block;
+    font-size: 17px;
+    font-family: 'NanumSquareNeobold';
+    margin-right: 2%;
+    margin-left: 0.7%;
+    text-align: center;
+    border-bottom: #FFDE80 3px solid;
+    padding: 1.3%;
+    width: 5%;
+  }
+
+  .content-label{
+    display: inline-block;
+    font-size: 17px;
+    font-family: 'NanumSquareNeobold';
+    margin-left: 2%;
+    text-align: center;
+    width: 5%;
+    padding: 1.2%;
+    margin-bottom: 1.5%;
+    border-bottom: #FFDE80 3px solid;
+
+  }
+
+input[type="text"]{
+  width:80%;
+    padding: 15px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    box-shadow: 1px 2px 5px  rgba(0, 0, 0, 0.1);
+    resize: vertical;
+}
+
+textarea {
+    margin-left: 1%;
+    width: 93%;
+    padding: 20px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    box-shadow: 1px 2px 5px  rgba(0, 0, 0, 0.1);
+    resize: vertical;
+  }
+
+  .button-group {
+    text-align: center;
+    font-family: 'NanumSquareNeo';
+  
+  }
+
+  .btn-primary,
+  .btn-secondary {
+    font-family: 'NanumSquareNeoExtraBold';
+    font-size: 16px;
+    width: 12%;
+    padding: 1.5%;
+    margin: 0 10px;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
+  }
+
+  .btn-primary {
+    background-color: #FFDE80;
+    color: #605548;
+    transition: box-shadow 0.3s ease;
+  }
+  .btn-primary:hover{
+    box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.4);
+  }
+
+  .btn-secondary {
+    background-color: rgb(96, 85, 72,0.8);
+    color: #ffffff;
+    transition: box-shadow 0.3s ease;
+  }
+  
+  .btn-secondary:hover {
+    box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.4);
+  }
+
+  @media  screen and (max-width: 1500px){}
 </style>
